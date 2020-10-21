09bac9427c78d7b94b58146ee0572d5ded6a0acf
oversv <albertomdv@gmail.com>
2020-10-21 11:44:36 +0100

    Updates

---
+++ precache-manifest.a89661a13995ee0482724c377122b61a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e546731bd1338998f2e70130102b89a4",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/index.html"
+  },
+  {
+    "revision": "dea237fe02160275e8cf",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/css/main.f0b4bdd0.chunk.css"
+  },
+  {
+    "revision": "7e226f2358f0f79a8450",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/js/2.88ab25b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/js/2.88ab25b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea237fe02160275e8cf",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/js/main.9027b7f5.chunk.js"
+  },
+  {
+    "revision": "70052473e21d659abfbe",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/js/runtime-main.cac4cdd6.js"
+  },
+  {
+    "revision": "a2580ee90560fc8105341c37e970974a",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/bg-pattern-desktop.a2580ee9.svg"
+  },
+  {
+    "revision": "40d8475d9d575db6640d7db693df351f",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/bg-pattern-mobile.40d8475d.svg"
+  },
+  {
+    "revision": "ba0110cbe699febf9b554567b3c319ab",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/icon-arrow-down.ba0110cb.svg"
+  },
+  {
+    "revision": "a7ef46ea71d879197334a0d3b77938b2",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/illustration-box-desktop.a7ef46ea.svg"
+  },
+  {
+    "revision": "44e3065026b08ed32221e1916da7484e",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/illustration-woman-online-desktop.44e30650.svg"
+  },
+  {
+    "revision": "daf669348cc4af537c1224c0b07de357",
+    "url": "/FrontEndMentor-React-FAQ-accordion-card/static/media/illustration-woman-online-mobile.daf66934.svg"
+  }
+]);
